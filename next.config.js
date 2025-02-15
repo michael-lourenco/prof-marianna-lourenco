3f4254a5aece30aaead40a98a44e74d30ed470ac
Michael Lourenco <michael.lourenco@pixter.com.br>
2025-02-15 16:36:15 -0300

    add dist

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  output: 'export',
+  distDir: 'dist',
+  images: {
+    unoptimized: true, // Desativa a otimização de imagens para exportação estática
+  },
 }
 
 module.exports = nextConfig
