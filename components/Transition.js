ac72baef7180577819bfbdadbba14ee4a39de4f8
Michael Lourenco <michael.lourenco@pixter.com.br>
2025-02-16 03:32:24 -0300

    change transition colors

--- components/Transition.js
+++ components/Transition.js
@@ -21,7 +21,7 @@ const Transition = () => {
   return (
     <>
       <motion.div
-        className='fixed top-0 bottom-0 right-full w-screen h-screen z-30 bg-[#2e2257]'
+        className='fixed top-0 bottom-0 right-full w-screen h-screen z-30 bg-[#bd6905]'
         variants={transitionVariants}
         initial='initial'
         animate='animate'
@@ -29,7 +29,7 @@ const Transition = () => {
         transition={{ delay: 0.2, duration: 0.6, ease: 'easeInOut' }}
       ></motion.div>
       <motion.div
-        className='fixed top-0 bottom-0 right-full w-screen h-screen z-20 bg-[#3b2d71]'
+        className='fixed top-0 bottom-0 right-full w-screen h-screen z-20 bg-[#d4a823]'
         variants={transitionVariants}
         initial='initial'
         animate='animate'
@@ -37,7 +37,7 @@ const Transition = () => {
         transition={{ delay: 0.4, duration: 0.6, ease: 'easeInOut' }}
       ></motion.div>
       <motion.div
-        className='fixed top-0 bottom-0 right-full w-screen h-screen z-10 bg-[#4b3792]'
+        className='fixed top-0 bottom-0 right-full w-screen h-screen z-10 bg-[#f3dcaf]'
         variants={transitionVariants}
         initial='initial'
         animate='animate'
