c29c7630035525f8801cfce23187a9f5bc1a90b7
Michael Lourenco <michael.lourenco@pixter.com.br>
2025-02-20 11:14:11 -0300

    add whatsapp phone

--- components/Socials.js
+++ components/Socials.js
@@ -1,43 +1,38 @@
-// links
-import Link from 'next/link';
+import Link from "next/link"
 
-// icons
-import {
-  RiYoutubeLine,
-  RiInstagramLine,
-  RiFacebookLine,
-  RiDribbbleLine,
-  RiBehanceLine,
-  RiPinterestLine,
-  RiWhatsappLine,
-} from 'react-icons/ri';
+import { RiYoutubeLine, RiInstagramLine, RiWhatsappLine } from "react-icons/ri"
 
 const Socials = () => {
+  const phoneNumber = "5521964362490" // Replace with the actual phone number
+  const whatsappUrl = `https://wa.me/${phoneNumber}`
+
   return (
-    <div className='flex items-center gap-x-5 text-lg'>
-      <Link href={'https://www.youtube.com/@profmariaNNalourenco'} className='hover:text-accent transition-all duration-300'>
+    <div className="flex items-center gap-x-5 text-lg">
+      <Link
+        href={"https://www.youtube.com/@profmariaNNalourenco"}
+        target="_blank"
+        className="hover:text-accent transition-all duration-300"
+      >
         <RiYoutubeLine />
       </Link>
-      <Link href={''} className='hover:text-accent transition-all duration-300'>
+      <Link
+        href={whatsappUrl}
+        className="hover:text-accent transition-all duration-300"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <RiWhatsappLine />
       </Link>
-      {/* <Link href={''} className='hover:text-accent transition-all duration-300'>
-        <RiFacebookLine />
-      </Link> */}
-      <Link href={'https://www.instagram.com/profmariannalourenco/'} className='hover:text-accent transition-all duration-300'>
+      <Link
+        href={"https://www.instagram.com/profmariannalourenco/"}
+        target="_blank"
+        className="hover:text-accent transition-all duration-300"
+      >
         <RiInstagramLine />
       </Link>
-      {/* <Link href={''} className='hover:text-accent transition-all duration-300'>
-        <RiDribbbleLine />
-      </Link>
-      <Link href={''} className='hover:text-accent transition-all duration-300'>
-        <RiBehanceLine />
-      </Link>
-      <Link href={''} className='hover:text-accent transition-all duration-300'>
-        <RiPinterestLine />
-      </Link> */}
     </div>
-  );
-};
+  )
+}
 
-export default Socials;
+export default Socials
+
